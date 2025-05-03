435cd005dfbcdf8263684edef35e3107c5d09057
Maksim Andreev <Dayman88@yandex.ru>
2025-05-03 22:48:36 +0300

    v 0.8

--- webpack.config.js
+++ webpack.config.js
@@ -6,62 +6,61 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/scripts/index.js',
+  entry: "./src/scripts/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/mesto-project-ff/",
     clean: true,
-    assetModuleFilename: 'images/[name][ext]'
+    assetModuleFilename: "images/[name][ext]",
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,               // теперь без include — все .css
+        test: /\.css$/i, // теперь без include — все .css
         use: [
           MiniCssExtractPlugin.loader, // вытаскиваем CSS в styles.css
-          'css-loader'                 // обрабатываем @import и url()
-        ]
+          "css-loader", // обрабатываем @import и url()
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'images/[name][ext]' }
+        type: "asset/resource",
+        generator: { filename: "images/[name][ext]" },
       },
       {
         test: /\.(woff2?|ttf|eot)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'fonts/[name][ext]' }
-      }
-    ]
+        type: "asset/resource",
+        generator: { filename: "fonts/[name][ext]" },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/pages/index.html'),
-      filename: 'index.html',
-      inject: 'body'
+      template: path.resolve(__dirname, "src/pages/index.html"),
+      filename: "index.html",
+      inject: "body",
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: "styles.css",
     }),
-  new CopyWebpackPlugin({
-    patterns: [
-      { from: 'src/images', to: 'images' }
-    ]
-  }),
-  new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src/pages/index.html'),
-    filename: 'index.html',
-    inject: 'body'
-  }),
-  new MiniCssExtractPlugin({
-    filename: 'styles.css'
-  })
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/images", to: "images" }],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/pages/index.html"),
+      filename: "index.html",
+      inject: "body",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
   ],
   devServer: {
-    static: './dist',
+    static: "./dist",
     open: true,
     hot: true,
-    port: 8080
+    port: 8080,
   },
-  mode: 'development'
+  mode: "development",
 };
